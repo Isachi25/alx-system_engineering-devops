7e5476b541e206fed76ef7c24a535065c827a48b
Isachi25 <sharonachiengmasiga@gmail.com>
2024-04-24 15:44:49 -0700

    Execute a command

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,8 @@
-# Executes a command
+# Kills a process named killmenow using pkill
 
-exec { 'pkill killmenow':
-  command => '/usr/bin:/usr/sbin:/bin'
+exec { 'killmenow':
+  command     => 'pkill killmenow',
+  path        => ['/usr/bin', '/usr/sbin', '/bin'],
+  refreshonly => true,
 }
 
