a47999a878d66862ef37d576a098b26da6661a65
Isachi25 <sharonachiengmasiga@gmail.com>
2024-04-29 17:29:14 -0700

    Web server

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,21 @@
+# puppet: install and configure Nginx
+
+package { 'nginx':
+  ensure => installed,
+}
+
+file_line { 'server_config':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://stackoverflow.com/ permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
