8d22e94386f678f3b224d0552c4915c11cab58cf
Isachi25 <sharonachiengmasiga@gmail.com>
2024-06-06 17:04:36 -0700

    1-top_ten.py correction

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,15 +1,24 @@
 #!/usr/bin/python3
-"""Prints top 10 posts"""
+'''
+    this module contains the function top_ten
+'''
 import requests
+from sys import argv
 
 
 def top_ten(subreddit):
-    """Prints top 10 posts"""
-    URL = 'http://reddit.com/r/{}/hot.json'.format(subreddit)
-    HEADERS = {'User-agent': 'Unix:0-subs:v1'}
-    response = requests.get(URL, headers=HEADERS)
-    posts = response.json().get('data', {}).get('children', {})
-    if response.status_code != 200 or not posts:
-        return print('None')
-    for post in posts[0:10]:
-        print(post.get('data', {}).get('title'))
+    '''
+        returns the top ten posts for a given subreddit
+    '''
+    user = {'User-Agent': 'Lizzie'}
+    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
+                       .format(subreddit), headers=user).json()
+    try:
+        for post in url.get('data').get('children'):
+            print(post.get('data').get('title'))
+    except Exception:
+        print(None)
+
+
+if __name__ == "__main__":
+    top_ten(argv[1])
